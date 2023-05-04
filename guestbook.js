bb33ba407526cd0d0789e8ea507dd5965d69f3d8
mulhermaonocodigo <maysa.macedo>
2023-05-03 23:52:41 -0400

    update links

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/c1f7dafa-24cb-485c-aa0e-d7476654c565/guestbook/';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/c1f7dafa-24cb-485c-aa0e-d7476654c565/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
